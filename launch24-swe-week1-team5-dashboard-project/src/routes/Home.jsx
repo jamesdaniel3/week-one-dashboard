88071f7686cfa14e08cc3ec5aaad05c21b2a9d4f
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-05-20 17:09:11 -0400

    moved functionality to main

--- launch24-swe-week1-team5-dashboard-project/src/routes/Home.jsx
+++ launch24-swe-week1-team5-dashboard-project/src/routes/Home.jsx
@@ -1,9 +1,22 @@
 import NavBar from "../components/Navbar.jsx";
+import { CourseCard } from '../components/CourseCard'
 const Main = () => {
+    var course = {
+        "color": "blue",
+        "route": "",
+        "title":"Calculus II",
+        "category":"MATH",
+        "id": "2140",
+        "enrollment": 0,
+        "enrollment_cap": 100,
+        "image":"src/assets/testimg.jpg",
+        "avg_grade":86.0,
+        "desc":"Calculus II is an exploration of deeper and more advanced mathematics. Students in this course will learn everything ranging from simple derivation to much more advanced integration methods.",
+    }
     return (
         <>
             <NavBar />
-            <p>This is the home page</p>
+            <CourseCard course={course}/>
         </>
     )
 };
