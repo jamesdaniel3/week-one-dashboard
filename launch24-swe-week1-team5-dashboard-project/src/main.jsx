0c453815ce705397ae39fb2e01d1c3ef4695d4d5
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-05-20 13:27:55 -0400

    created home page and linked to it

--- launch24-swe-week1-team5-dashboard-project/src/main.jsx
+++ launch24-swe-week1-team5-dashboard-project/src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+import Home from "./routes";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Home />,
     },
 ]);
 
---
+++ launch24-swe-week1-team5-dashboard-project/src/routes/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-douter-dom";
+
+const Main = () => {
+    return (
+        <>
+            <p>This is the home page</p>
+        </>
+    )
+};
+
+export default Main;
