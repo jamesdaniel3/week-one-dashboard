409a36d555a9ca8c7026f3bc46a36cb4c5680f74
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-05-20 13:28:27 -0400

    removed unused import

--- launch24-swe-week1-team5-dashboard-project/src/main.jsx
+++ launch24-swe-week1-team5-dashboard-project/src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider,} from "react-router-dom";
-import Home from "./routes";
+import Home from "./routes/Home";
 
 const router = createBrowserRouter([
     {
