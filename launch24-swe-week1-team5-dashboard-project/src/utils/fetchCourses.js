4401dbb602ca7efb2a952f114bb502e44564d54a
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-05-21 13:08:14 -0400

    super quick refactor

--- launch24-swe-week1-team5-dashboard-project/src/utils/fetchCourses.js
+++ launch24-swe-week1-team5-dashboard-project/src/utils/fetchCourses.js
@@ -1,11 +1,10 @@
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase";
+import {collection, getDocs} from "firebase/firestore";
+import {db} from "../firebase";
 
 const fetchCourses = async () => {
     const coursesCollection = collection(db, "courses");
     const coursesSnapshot = await getDocs(coursesCollection);
-    const coursesList = coursesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    return coursesList;
+    return coursesSnapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
 };
 
 export default fetchCourses;
