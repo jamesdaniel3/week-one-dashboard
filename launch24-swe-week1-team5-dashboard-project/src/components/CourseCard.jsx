45418780cdc3fd6c306b7fa6f5ff0f1c5b9400f7
Elliott Hansen <elliott@redvw.com>
2024-05-21 13:35:21 -0400

    updated cards

--- launch24-swe-week1-team5-dashboard-project/src/components/CourseCard.jsx
+++ launch24-swe-week1-team5-dashboard-project/src/components/CourseCard.jsx
@@ -35,47 +35,5 @@ export const CourseCard = ({course=null, courseid=null}) => {
                 </a>
             </>
         )
-        // else if (course.avg_grade >= 70) {
-        //     return (
-        //         <>
-        //             <div className={"course-card " + course.color}>
-        //                 <div className='title'>
-        //                 <h1>
-        //                     {course.title}
-        //                 </h1>
-        //                 </div>
-        //                 <p className='description' maxlength='50'>
-        //                     {course.desc}
-        //                 </p>
-        //                 <div className="course-card-footer">
-        //                     <div className='grade-overview medium'>
-        //                         <span> {course.avg_grade}% </span>
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </>
-        //     )
-        // }
-        // else {
-        //     return (
-        //         <>
-        //             <div className={"course-card " + course.color}>
-        //                 <div className='title'>
-        //                 <h1>
-        //                     {course.title}
-        //                 </h1>
-        //                 </div>
-        //                 <p className='description' maxlength='50'>
-        //                     {course.desc}
-        //                 </p>
-        //                 <div className="course-card-footer">
-        //                     <div className='grade-overview poor'>
-        //                         <span> {course.avg_grade}% </span>
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </>
-        //     )
-        // }
     }
 }
