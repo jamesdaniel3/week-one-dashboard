0f739e69efd76b7794c3f9193177cee2284d81ba
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-05-21 22:43:53 -0400

    put calendar in with FullCalendar

--- launch24-swe-week1-team5-dashboard-project/src/routes/CalendarPage.jsx
+++ launch24-swe-week1-team5-dashboard-project/src/routes/CalendarPage.jsx
@@ -1,10 +1,11 @@
 import NavBar from "../components/Navbar.jsx";
+import Calendar from "../components/Calendar.jsx";
 
 const Main = () => {
     return (
         <>
             <NavBar />
-            <p>This is the calendar page</p>
+            <Calendar />
         </>
     )
 };
